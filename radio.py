145a4396f981ba1f4258989aa00ccf4274027cd0
Stefan Schroeder <ondekoza@gmail.com>
2022-11-14 21:41:05 +0100

    extract json

--- radio.py
+++ radio.py
@@ -11,32 +11,16 @@ import time, shlex
 
 from RPi import GPIO
 from curses import wrapper
+from pathlib import Path
 
-current_channel = 0
+jsondir = Path(__file__).parent
 
-json_string = """
-{ 
-    "radio": [ 
-        "mplayer -playlist http://www.ndr.de/resources/metadaten/audio/m3u/ndrinfo.m3u"
-        , "mplayer https://st01.dlf.de/dlf/01/128/mp3/stream.mp3"
-        , "mplayer https://wdr-wdr5-live.icecastssl.wdr.de/wdr/wdr5/live/mp3/128/stream.mp3"
-        , "mplayer http://stream.live.vc.bbcmedia.co.uk/bbc_world_service"
-    ],
-    "amixer": "Headphone",
-    "current": 0,
-    "volume": 60
-} 
-"""
-
-#data = json.loads(json_string)
-data = json.load("radio.json")
+with open(str(jsondir) + "/" + "radio.json", 'r') as f:
+    data = json.load(f)
 
 CLK = 5
 DT = 7
 SW = 32
-
-def RotButton(dummy):
-    SetRadioChannelUp()
 
 def CLKClicked(channel):
     CLKState = GPIO.input(CLK)
@@ -62,16 +46,12 @@ def SetRadioChannel():
     my_env["MPLAYER_VERBOSE"] = "-1"
     subprocess.Popen(args, env=my_env)
 
-def SetRadioChannelIndex(c):
-    data["current"] = c
-    SetRadioChannel()
-
 def SetRadioChannelUp():
     data["current"] = ( data["current"] + 1 ) % len(data["radio"])
     SetRadioChannel()
 
 def no_curses_main():
-    SetRadioChannelIndex(data["current"])
+    SetRadioChannel()
     loop = asyncio.get_event_loop()
     try:
         loop.run_forever()
@@ -86,7 +66,7 @@ GPIO.setup(SW, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.add_event_detect(CLK, GPIO.FALLING, callback=CLKClicked, bouncetime=300)
 GPIO.add_event_detect(DT, GPIO.FALLING, callback=DTClicked, bouncetime=300)
-GPIO.add_event_detect(SW, GPIO.FALLING, callback=RotButton, bouncetime=250)
+GPIO.add_event_detect(SW, GPIO.FALLING, callback=SetRadioChannelUp, bouncetime=250)
 
 clamp = lambda n: max(min(100, n), 0)
 
